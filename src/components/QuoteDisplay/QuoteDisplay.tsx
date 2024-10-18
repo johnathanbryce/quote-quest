9213091258a6c063e4187edeea56fa580e6a1357
johnathanbryce <johnathanbryce@gmail.com>
2024-10-18 11:27:23 -0700

    feat: basic styling applied to quote display section

--- src/components/QuoteDisplay/QuoteDisplay.tsx
+++ src/components/QuoteDisplay/QuoteDisplay.tsx
@@ -3,7 +3,17 @@ import { useGame } from "../../context/GameContext";
 
 const QuoteDisplay = () => {
   const { currentQuote } = useGame();
-  return <div className={styles.quote_container}>{currentQuote.quote}</div>;
+  return (
+    <div className={styles.quote_container}>
+      <div className={styles.quotes_wrapper}>
+        <span className={styles.opening_quote}>"</span>
+        <blockquote>
+          <p>{currentQuote.quote}</p>
+        </blockquote>
+        <span className={styles.closing_quote}>"</span>
+      </div>
+    </div>
+  );
 };
 
 export default QuoteDisplay;
