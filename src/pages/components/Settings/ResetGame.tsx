d9fd1883c2778d17f9eaf7c11cd0fa66519407a2
johnathanbryce <johnathanbryce@gmail.com>
2024-10-25 11:11:22 -0700

    feat: added reset button to settings panel

---
+++ src/pages/components/Settings/ResetGame.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Button from "../Button/Button";
+
+type ResetGameProps = {
+  handleResetGame: () => void;
+};
+
+const ResetGame = ({ handleResetGame }: ResetGameProps) => {
+  return (
+    <div>
+      <Button onClick={handleResetGame}>Reset Game</Button>
+    </div>
+  );
+};
+
+export default ResetGame;
